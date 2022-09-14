9e83f9a0675c9e5f2f4ae2068d21245092a3f9bb
Sandra <sandraliliana91@hotmail.com>
2022-09-14 05:17:58 -0500

    conectado a archivo js y css

--- Script_MinTiCode.js
+++ Script_MinTiCode.js
@@ -1,1 +1,7 @@
-alert ("Bienvenidos")
+//alert ("Bienvenidos")
+
+document.writeln ("Datos de la empresa");
+
+document.writeln ("Ubicacion")
+document.writeln ("telefono")
+document.writeln ("redes sociales");
